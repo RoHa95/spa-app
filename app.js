02517e260d6f2a5954e194f7819ae798ccdc468c
Roqhaye Halimi <roqiahalimi95@gmail.com>
2023-12-13 16:29:43 -0800

    create pages content

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+import Dashbord from "./pages/Dashbord.js";
+import Posts from "./pages/Posts.js";
+import Products from "./pages/Products.js";
+
 function router(params) {
   const routes = [
     {
       path: "/",
-      view: () => console.log("dashbord page"),
+      view: Dashbord,
     },
     {
       path: "/posts",
-      view: () => console.log("posts page"),
+      view: Posts,
     },
     {
       path: "/products",
-      view: () => console.log("products page"),
+      view: Products,
     },
   ];
+  console.log("hi 1");
   const potentialRoutes = routes.map((item) => {
     return {
       route: item,
@@ -20,14 +25,17 @@ function router(params) {
     };
   });
   let match = potentialRoutes.find((item) => item.isMatch);
-  console.log(potentialRoutes);
-  console.log(match.route.view());
+  // console.log(potentialRoutes);
+  // console.log("hi 1");
+  // console.log(match.route.view());
   if (!match) {
     match = {
       route: { path: "/not-found", view: () => console.log("not-found page") },
       isMatch: true,
     };
   }
+  console.log(match.route.view());
+  document.querySelector("#app").innerHTML = match.route.view();
 }
 //2. push user to new rout
 function navigateTo(url) {
@@ -37,6 +45,7 @@ function navigateTo(url) {
 window.addEventListener("popstate", router);
 document.addEventListener("DOMContentLoaded", () => {
   document.body.addEventListener("click", (e) => {
+    console.log("hi load");
     if (e.target.hasAttribute("data-link")) {
       e.preventDefault();
       navigateTo(e.target.href);
